5a598adcee97c3f758b62c030b4482483f26fa45
Anvesh <anveshyadavmokam@gmail.com>
2024-12-12 15:29:24 +0530

    change font

--- src/server/api/auth/auth.js
+++ src/server/api/auth/auth.js
@@ -4,27 +4,11 @@ import dbConfig from "../../database/dbConfig.js";
 import jwt from "jsonwebtoken";
 import User from "../../database/models/user.js";
 import bcrypt from "bcrypt";
-
 dotenv.config();
 dbConfig();
 const router = express.Router();
 const generateToken = (payload, secret, expiresIn = "1d") => {
     return jwt.sign(payload, secret, { expiresIn });
-};
-
-const verifyToken = (req, res, next) => {
-    const token = req.cookies.token;
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized, no token found" });
-    }
-
-    try {
-        req.user = jwt.verify(token, process.env.JWT_SECRET);
-        next();
-    } catch (err) {
-        res.clearCookie("token");
-        return res.status(401).json({ message: "Unauthorized, invalid token" });
-    }
 };
 
 router.post('/signup', async (req, res) => {
